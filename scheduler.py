f7e2c99443ac1f61e6f00ecb87388ddd74b7bcf4
Adityarup Datta <76240466+addycracker@users.noreply.github.com>
2021-08-22 22:04:21 +0530

    Add files via upload

---
+++ scheduler.py
@@ -0,0 +1,51 @@
+import schedule
+import time
+from plyer import notification
+import playsound
+
+
+def water_reminder():
+
+    notification.notify(
+        title="**Please Drink Water Now!!",
+        message="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters)of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+        app_icon="D:\\pyproject_internhips\\notifier\\water.ico",
+
+    )
+    playsound.playsound("drink.mp3")
+
+def coding():
+
+    notification.notify(
+        title="**coding time**",
+        message="Its coding time get up and grab a cup of coffee and dig in",
+        app_icon="D:\\pyproject_internhips\\notifier\\coding.ico",
+    )
+    playsound.playsound('coding.mp3')
+
+def play():
+
+    notification.notify(
+        title="**playing time**",
+        message="Its playing time go outside and enjoy",
+        app_icon="D:\\pyproject_internhips\\notifier\\play.ico",
+    )
+    playsound.playsound('playing.mp3')
+
+def classes():
+
+    notification.notify(
+        title="**Class time**",
+        message="Hey, Your class will start soon. get up",
+        app_icon="D:\\pyproject_internhips\\notifier\\class.ico",
+    )
+    playsound.playsound('class.mp3')
+
+schedule.every().day.at("21:54").do(coding)
+schedule.every(30).minutes.do(water_reminder)
+schedule.every().day.at("21:55").do(play)
+schedule.every().day.at("21:56").do(classes)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
